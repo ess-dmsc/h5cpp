d3e77d6cfc74da2e7b9f0cac295f7c8f6c96763f
MGS <martukas@gmail.com>
2017-07-12 15:22:56 +0200

    using boost test framework

--- experiments/main.cpp
+++ experiments/main.cpp
@@ -1,8 +1,25 @@
-#include "object_id.h"
+#include <cstdlib>
+#include <hdf5.h>
 
-int main(int argc, char **argv)
+#define BOOST_TEST_MODULE MyTest
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+
+#define FILE "file.h5"
+
+// single file, multiple paths to same node
+// copy file, see if id's are the same
+// external link vs. native path
+// file & symbolic link
+
+BOOST_AUTO_TEST_CASE( my_test )
 {
-//  H5CC::exceptions_off();
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  hid_t file_id =
+      H5Fcreate(FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+  herr_t status = H5Fclose(file_id);
+
+  BOOST_CHECK_GE(status, 0);
+
+  boost::filesystem::remove(FILE);
 }
