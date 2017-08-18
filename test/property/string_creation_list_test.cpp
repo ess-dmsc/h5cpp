f9b9ee880902c952e6afda8d40c3affe82573ceb
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2017-08-18 14:29:18 +0200

    Implemented StringCreationList

--- test/property/string_creation_list_test.cpp
+++ test/property/string_creation_list_test.cpp
@@ -1,7 +1,7 @@
 //
 // (c) Copyright 2017 DESY,ESS
 //
-// This file is part of h5cpp.
+// This file is part of h5pp.
 //
 // This library is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as published
@@ -22,42 +22,30 @@
 // Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
 // Created on: Aug 18, 2017
 //
-#pragma once
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Testing string creation property list implementation
+#include <boost/test/unit_test.hpp>
 
-#include "list.hpp"
-#include "../type/types.hpp"
+#include <h5cpp/property/string_creation_list.hpp>
 
-namespace hdf5{
-namespace property{
+namespace prop = hdf5::property;
+namespace type = hdf5::type;
 
-class StringCreationList : public List
-{
-  public:
-    //!
-    //! \brief constructor
-    //!
-    StringCreationList();
+BOOST_AUTO_TEST_SUITE(StringCreationListTest)
 
-    //!
-    //! \brief destructor
-    //!
-    virtual ~StringCreationList();
+  BOOST_AUTO_TEST_CASE(test_default_construction)
+  {
+    prop::StringCreationList scl;
+    BOOST_CHECK(scl.get_class() == prop::kStringCreate);
+  }
 
-    //!
-    //! \brief get character encoding
-    //! @return current character encoding
-    //! \sa character_encoding
-    type::CharacterEncoding character_encoding() const;
+  BOOST_AUTO_TEST_CASE(test_encoding)
+  {
+    prop::StringCreationList scl;
+    BOOST_CHECK_NO_THROW(scl.character_encoding(type::CharacterEncoding::ASCII));
+    BOOST_CHECK(scl.character_encoding()==type::CharacterEncoding::ASCII);
 
-    //!
-    //! \brief set character encoding
-    //! @param encoding the required character encoding
-    //! \sa character_encoding
-    void character_encoding(type::CharacterEncoding encoding) const;
-
-  protected:
-    StringCreationList(const Class &plist_class);
-};
-
-}
-}
+    BOOST_CHECK_NO_THROW(scl.character_encoding(type::CharacterEncoding::UTF8));
+    BOOST_CHECK(scl.character_encoding()==type::CharacterEncoding::UTF8);
+  }
+BOOST_AUTO_TEST_SUITE_END()
