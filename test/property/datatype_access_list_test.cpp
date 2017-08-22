940a4890d4c506eb83771cbd6f6dea5a2a86a35c
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2017-08-22 13:20:48 +0200

    Added datatype access property list

---
+++ test/property/datatype_access_list_test.cpp
@@ -0,0 +1,41 @@
+//
+// (c) Copyright 2017 DESY,ESS
+//
+// This file is part of h5pp.
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor
+// Boston, MA  02110-1301 USA
+// ===========================================================================
+//
+// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Created on: Aug 22, 2017
+//
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE testing datatype access property list implementation
+#include <boost/test/unit_test.hpp>
+#include <h5cpp/property/datatype_access_list.hpp>
+#include <h5cpp/property/class.hpp>
+
+namespace pl = hdf5::property;
+
+BOOST_AUTO_TEST_SUITE(DatatypeAccessList_test)
+
+BOOST_AUTO_TEST_CASE(test_default_construction)
+{
+  pl::DatatypeAccessList dapl;
+  BOOST_CHECK(dapl.get_class() == pl::kDatatypeAccess);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
