6580728d173c7d46f40d26e1a55b2db8506bd5ed
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2017-08-24 16:14:28 +0200

    Added a first implementation for an HDF5 path

---
+++ src/include/h5cpp/path.hpp
@@ -0,0 +1,86 @@
+//
+// (c) Copyright 2017 DESY,ESS
+//
+// This file is part of h5cpp.
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor
+// Boston, MA  02110-1301 USA
+// ===========================================================================
+//
+// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Created on: Aug 24, 2017
+//
+#pragma once
+
+#include <list>
+#include <string>
+
+namespace hdf5 {
+
+//!
+//! \brief path to a node object
+//!
+class Path
+{
+  public:
+    using value_type = std::string;
+    using iterator = std::list<value_type>::iterator;
+    using const_iterator = std::list<value_type>::const_iterator;
+    using reverse_iterator = std::list<value_type>::reverse_iterator;
+    using const_reverse_iterator= std::list<value_type>::const_reverse_iterator;
+
+    Path();
+    explicit Path(const std::string &str);
+    Path(const Path &p) = default;
+
+    explicit operator std::string() const
+    {
+      return to_string();
+    }
+
+    size_t size() const;
+
+    const_iterator begin() const;
+    const_iterator end() const;
+    iterator begin();
+    iterator end();
+
+    reverse_iterator rbegin();
+    reverse_iterator rend();
+    const_reverse_iterator rbegin() const;
+    const_reverse_iterator rend() const;
+
+    void push_front(const value_type &link_name);
+    value_type pop_front();
+
+    void push_back(const value_type &link_name);
+    value_type pop_back();
+
+    bool is_absolute_path() const noexcept;
+    void is_absolute_path(bool value) noexcept;
+
+  private:
+    bool absolute_;
+    std::list<std::string> link_names_;
+
+    void from_string(const std::string &str);
+    std::string to_string() const;
+
+};
+
+Path operator+(const std::string &link_name,const Path &path);
+Path operator+(const Path &path,const std::string &link_name);
+
+} // namespace hdf5
---
+++ test/path_test.cpp
@@ -0,0 +1,109 @@
+//
+// (c) Copyright 2017 DESY,ESS
+//
+// This file is part of h5pp.
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor
+// Boston, MA  02110-1301 USA
+// ===========================================================================
+//
+// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Created on: Aug 24, 2017
+//
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE testing the path implementation
+#include <boost/test/unit_test.hpp>
+#include <h5cpp/path.hpp>
+
+BOOST_AUTO_TEST_SUITE(Path_test)
+
+BOOST_AUTO_TEST_CASE(test_default_construction)
+{
+  hdf5::Path p;
+  BOOST_CHECK_EQUAL(p.size(),0);
+  BOOST_CHECK(!p.is_absolute_path());
+
+}
+
+BOOST_AUTO_TEST_CASE(test_construction_from_string)
+{
+  hdf5::Path p("/hello/world/data");
+  BOOST_CHECK_EQUAL(p.size(),3);
+  BOOST_CHECK(p.is_absolute_path());
+
+  p = hdf5::Path("hello/world");
+  BOOST_CHECK_EQUAL(p.size(),2);
+  BOOST_CHECK(!p.is_absolute_path());
+
+  p = hdf5::Path("hello/world/instrument/data/");
+  BOOST_CHECK_EQUAL(p.size(),4);
+  BOOST_CHECK(!p.is_absolute_path());
+}
+
+BOOST_AUTO_TEST_CASE(test_conversion_to_string)
+{
+  hdf5::Path p("/hello/world/data");
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"/hello/world/data");
+
+  p = hdf5::Path("hello/world");
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"hello/world");
+
+  p = hdf5::Path("hello/world/instrument/data/");
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"hello/world/instrument/data");
+}
+
+BOOST_AUTO_TEST_CASE(test_conversion_from_list)
+{
+  std::list<std::string> l{"entry","instrument","detector"};
+  hdf5::Path p;
+  std::copy(l.begin(),l.end(),std::back_inserter(p));
+  BOOST_CHECK_EQUAL(p.size(),3);
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"entry/instrument/detector");
+}
+
+BOOST_AUTO_TEST_CASE(test_append_link_name)
+{
+  hdf5::Path p("/entry/instrument/detector");
+  p = p+"data";
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"/entry/instrument/detector/data");
+  BOOST_CHECK_EQUAL(p.size(),4);
+  BOOST_CHECK(p.is_absolute_path());
+
+  p = hdf5::Path("instrument/detector");
+  p = p + "metadata/date";
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"instrument/detector/metadata/date");
+  BOOST_CHECK_EQUAL(p.size(),4);
+  BOOST_CHECK(!p.is_absolute_path());
+}
+
+BOOST_AUTO_TEST_CASE(test_prepend_link_name)
+{
+  hdf5::Path p("instrument/detector");
+  p = "/entry" + p;
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"/entry/instrument/detector");
+  BOOST_CHECK(p.is_absolute_path());
+
+  p = hdf5::Path("detector/data");
+  p = "/entry/instrument/" + p;
+  BOOST_CHECK_EQUAL(static_cast<std::string>(p),"/entry/instrument/detector/data");
+  BOOST_CHECK(p.is_absolute_path());
+
+  p = hdf5::Path("entry/instrument");
+  p = "/" + p;
+  BOOST_CHECK_EQUAL(p.size(),2);
+  BOOST_CHECK(p.is_absolute_path());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
