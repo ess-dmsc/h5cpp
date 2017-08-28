90b3693871ed6d3af5a77fb019b3cdfb93f6a624
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2017-08-28 12:51:13 +0200

    added dataset creation property list

--- src/include/h5cpp/property/dataset_creation_list.hpp
+++ src/include/h5cpp/property/dataset_creation_list.hpp
@@ -24,37 +24,104 @@
 //
 #pragma once
 
+#include <iostream>
 #include "object_creation_list.hpp"
 #include "../types.hpp"
+#include "../datatype/datatype.hpp"
+#include "../datatype/type_trait.hpp"
 
 namespace hdf5 {
 namespace property {
 
+enum class DatasetFillValueStatus : std::underlying_type<H5D_fill_value_t>::type
+{
+  UNDEFINED = H5D_FILL_VALUE_UNDEFINED,
+  DEFAULT   = H5D_FILL_VALUE_DEFAULT,
+  USER_DEFINED = H5D_FILL_VALUE_USER_DEFINED
+};
+
+std::ostream &operator<<(std::ostream &stream,const DatasetFillValueStatus &status);
+
+enum class DatasetFillTime : std::underlying_type<H5D_fill_time_t>::type
+{
+  IFSET = H5D_FILL_TIME_IFSET,
+  ALLOC = H5D_FILL_TIME_ALLOC,
+  NEVER = H5D_FILL_TIME_NEVER
+};
+
+std::ostream &operator<<(std::ostream &stream,const DatasetFillTime &time);
+
+enum class DatasetAllocTime : std::underlying_type<H5D_alloc_time_t>::type
+{
+  DEFAULT = H5D_ALLOC_TIME_DEFAULT,
+  EARLY   = H5D_ALLOC_TIME_EARLY,
+  INCR    = H5D_ALLOC_TIME_INCR,
+  LATE    = H5D_ALLOC_TIME_LATE
+};
+
+std::ostream &operator<<(std::ostream &stream,const DatasetAllocTime &time);
+
+enum class DatasetLayout : std::underlying_type<H5D_layout_t>::type
+{
+  COMPACT    = H5D_COMPACT,
+  CONTIGUOUS = H5D_CONTIGUOUS,
+  CHUNKED    = H5D_CHUNKED,
+#if H5_VERSION_GE(1,10,0)
+  VIRTUAL    = H5D_VIRTUAL
+#endif
+};
+
+std::ostream &operator<<(std::ostream &stream,const DatasetLayout &layout);
+
 class DatasetCreationList : public ObjectCreationList
 {
   public:
-    enum class Layout : std::underlying_type<H5D_layout_t>::type
-    {
-      COMPACT    = H5D_COMPACT,
-      CONTIGUOUS = H5D_CONTIGUOUS,
-      CHUNKED    = H5D_CHUNKED,
-#if H5_VERSION_GE(1,10,0)
-      VIRTUAL    = H5D_VIRTUAL
-#endif
-    };
 
     DatasetCreationList();
     ~DatasetCreationList();
 
-    void layout(Layout layout) const;
-    Layout layout() const;
+    void layout(DatasetLayout layout) const;
+    DatasetLayout layout() const;
 
     void chunk(const hdf5::Dimensions &chunk_dims) const;
     hdf5::Dimensions chunk() const;
 
 
+    template<typename T>
+    void fill_value(const T &value,
+                    const datatype::Datatype &type=datatype::TypeTrait<T>::create()) const;
+    template<typename T>
+    T fill_value(const datatype::Datatype &type=datatype::TypeTrait<T>::create()) const;
 
+    DatasetFillValueStatus fill_value_status() const;
+
+    void fill_time(DatasetFillTime time) const;
+    DatasetFillTime fill_time() const;
+
+    void allocation_time(DatasetAllocTime time) const;
+    DatasetAllocTime allocation_time() const;
 };
+
+template<typename T>
+void DatasetCreationList::fill_value(const T &value,const datatype::Datatype &type) const
+{
+  if(H5Pset_fill_value(static_cast<hid_t>(*this),
+                       static_cast<hid_t>(type),&value)<0)
+  {
+    throw std::runtime_error("Failure to set fill value for dataset!");
+  }
+}
+
+template<typename T>
+T DatasetCreationList::fill_value(const datatype::Datatype &type) const
+{
+  T value;
+  if(H5Pget_fill_value(static_cast<hid_t>(*this),static_cast<hid_t>(type),&value)<0)
+  {
+    throw std::runtime_error("Failure retrieving the fill value for a dataset!");
+  }
+  return value;
+}
 
 } // namespace property
 } // namespace hdf5
--- src/property/dataset_creation_list.cpp
+++ src/property/dataset_creation_list.cpp
@@ -30,6 +30,65 @@
 namespace hdf5 {
 namespace property {
 
+std::ostream &operator<<(std::ostream &stream,const DatasetFillValueStatus &status)
+{
+  switch(status)
+  {
+    case DatasetFillValueStatus::UNDEFINED:
+      return stream<<"UNDEFINED";
+    case DatasetFillValueStatus::DEFAULT:
+      return stream<<"DEFAULT";
+    case DatasetFillValueStatus::USER_DEFINED:
+      return stream<<"USER_DEFINED";
+  }
+}
+
+std::ostream &operator<<(std::ostream &stream,const DatasetFillTime &time)
+{
+  switch(time)
+  {
+    case DatasetFillTime::IFSET:
+      return stream<<"IFSET";
+    case DatasetFillTime::ALLOC:
+      return stream<<"ALLOC";
+    case DatasetFillTime::NEVER:
+      return stream<<"NEVER";
+  }
+}
+
+std::ostream &operator<<(std::ostream &stream,const DatasetAllocTime &time)
+{
+  switch(time)
+  {
+    case DatasetAllocTime::DEFAULT:
+      return stream<<"DEFAULT";
+    case DatasetAllocTime::EARLY:
+      return stream<<"EARLY";
+    case DatasetAllocTime::INCR:
+      return stream<<"INCR";
+    case DatasetAllocTime::LATE:
+      return stream<<"LATE";
+  }
+}
+
+std::ostream &operator<<(std::ostream &stream,const DatasetLayout &layout)
+{
+  switch(layout)
+  {
+    case DatasetLayout::COMPACT:
+      return stream<<"COMPACT";
+    case DatasetLayout::CONTIGUOUS:
+      return stream<<"CONTIGUOUS";
+    case DatasetLayout::CHUNKED:
+      return stream<<"CHUNKED";
+#if H5_VERSION_GE(1,10,0)
+    case DatasetLayout::VIRTUAL:
+      return stream<<"VIRTUAL";
+#endif
+  }
+}
+
+
 DatasetCreationList::DatasetCreationList():
     ObjectCreationList(kDatasetCreate)
 {}
@@ -37,7 +96,7 @@ DatasetCreationList::DatasetCreationList():
 DatasetCreationList::~DatasetCreationList()
 {}
 
-void DatasetCreationList::layout(Layout layout) const
+void DatasetCreationList::layout(DatasetLayout layout) const
 {
   if(H5Pset_layout(static_cast<hid_t>(*this),static_cast<H5D_layout_t>(layout))<0)
   {
@@ -45,19 +104,19 @@ void DatasetCreationList::layout(Layout layout) const
   }
 }
 
-DatasetCreationList::Layout DatasetCreationList::layout() const
+DatasetLayout DatasetCreationList::layout() const
 {
-  switch(H5Pget_layout)
+  switch(H5Pget_layout(static_cast<hid_t>(*this)))
   {
     case H5D_COMPACT:
-      return Layout::COMPACT;
+      return DatasetLayout::COMPACT;
     case H5D_CONTIGUOUS:
-      return Layout::CONTIGUOUS;
+      return DatasetLayout::CONTIGUOUS;
     case H5D_CHUNKED:
-      return Layout::CHUNKED;
+      return DatasetLayout::CHUNKED;
 #if H5_VERSION_GE(1,10,0)
     case H5D_VIRTUAL:
-      return Layout::VIRTUAL;
+      return DatasetLayout::VIRTUAL;
 #endif
     default:
       throw std::runtime_error("Failure retrieving the dataset layout!");
@@ -89,6 +148,53 @@ Dimensions DatasetCreationList::chunk() const
   return buffer;
 }
 
+DatasetFillValueStatus DatasetCreationList::fill_value_status() const
+{
+  H5D_fill_value_t status;
+  if(H5Pfill_value_defined(static_cast<hid_t>(*this),&status)<0)
+  {
+    throw std::runtime_error("Failure obtaining the fill value status!");
+  }
+  return static_cast<DatasetFillValueStatus>(status);
+}
+
+void DatasetCreationList::fill_time(DatasetFillTime time) const
+{
+  if(H5Pset_fill_time(static_cast<hid_t>(*this),
+                      static_cast<H5D_fill_time_t>(time))<0)
+  {
+    throw std::runtime_error("Failure setting the fill time for the dataset!");
+  }
+}
+
+DatasetFillTime DatasetCreationList::fill_time() const
+{
+  H5D_fill_time_t buffer;
+  if(H5Pget_fill_time(static_cast<hid_t>(*this),&buffer)<0)
+  {
+    throw std::runtime_error("Failure retrieving dataset fill time!");
+  }
+  return static_cast<DatasetFillTime>(buffer);
+}
+
+void DatasetCreationList::allocation_time(DatasetAllocTime time) const
+{
+  if(H5Pset_alloc_time(static_cast<hid_t>(*this),
+                       static_cast<H5D_alloc_time_t>(time))<0)
+  {
+    throw std::runtime_error("Failure setting dataset allocation time!");
+  }
+}
+
+DatasetAllocTime DatasetCreationList::allocation_time() const
+{
+  H5D_alloc_time_t buffer;
+  if(H5Pget_alloc_time(static_cast<hid_t>(*this),&buffer)<0)
+  {
+    throw std::runtime_error("Failure retrieving dataset allocation time!");
+  }
+  return static_cast<DatasetAllocTime>(buffer);
+}
 
 } // namespace property
 } // namespace hdf5
