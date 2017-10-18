6bd3ec0c0abfa1a1044370990d230a7ad6422be9
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2017-10-18 15:27:05 +0200

    Check for Debian distribution
    Added some CMake code to distinguish between different Debian 
    distributions.
     
    Update #53

--- cmake/DebianGTestFix.cmake
+++ cmake/DebianGTestFix.cmake
@@ -1,6 +1,17 @@
 include(ExternalProject)
 
-set(GTEST_SOURCE_DIR /usr/src/googletest/googletest)
+execute_process(COMMAND ${LSB_RELEASE} -cs 
+                OUTPUT_VARIABLE LSB_CODENAME 
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+                
+if(LSB_CODENAME MATCHES stretch)
+	set(GTEST_SOURCE_DIR /usr/src/googletest/googletest)
+elseif(LSB_CODENAME MATCHES jessie)
+	set(GTEST_SOURCE_DIR /usr/src/gtest)
+else()
+	message(FATAL_ERROR "Could not identify Debian distribution!")
+endif()
+
 set(GTEST_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/googletest-build)
 add_subdirectory(${GTEST_SOURCE_DIR} ${GTEST_BINARY_DIR})
 set(GTEST_INCLUDE_DIRS ${GTEST_SOURCE_DIR}/include)
