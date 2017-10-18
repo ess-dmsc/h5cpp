501add834d5d4c606e83d281ac537e1ad3e91a37
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2017-10-18 14:15:58 +0200

    Including googletest as a subproject
    This seems to work much better. We can build the code now but still
    do not have tests on the command line.
    
    Update #53

--- cmake/DebianGTestFix.cmake
+++ cmake/DebianGTestFix.cmake
@@ -1,12 +1,7 @@
 include(ExternalProject)
 
-ExternalProject_Add(googletest
-    DEPENDS h5cpp_shared
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/googletest
-	SOURCE_DIR /usr/src/googletest/googletest
-	CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}i -DBUILD_SHARED_LIBS=ON
-	           -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/googletest
-	INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/googletest
-	)
-	
-set(GTEST_ROOT ${CMAKE_CURRENT_BINARY_DIR}/googltest)
+set(GTEST_SOURCE_DIR /usr/src/googletest/googletest)
+set(GTEST_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/googletest-build)
+add_subdirectory(${GTEST_SOURCE_DIR} ${GTEST_BINARY_DIR})
+set(GTEST_INCLUDE_DIRS ${GTEST_SOURCE_DIR}/include)
+set(GTEST_LIBRARIES gtest gtest_main)
