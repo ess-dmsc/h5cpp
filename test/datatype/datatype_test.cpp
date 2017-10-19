45a17b9796accf71fb92fb11c8e865989e48427a
MGS <martukas@gmail.com>
2017-10-19 23:34:25 +0200

    improved test coverage for Datatype #1

--- test/datatype/datatype_test.cpp
+++ test/datatype/datatype_test.cpp
@@ -63,37 +63,51 @@ TEST(Datatype, Comparators)
   EXPECT_EQ(a, b);
   b = Datatype(ObjectHandle(H5Tcopy(H5T_NATIVE_FLOAT)));
   EXPECT_NE(a, b);
+
+  Datatype c;
+  EXPECT_THROW((a == c), std::runtime_error);
 }
 
 TEST(Datatype, Classes)
 {
   Datatype a;
 //  EXPECT_TRUE(a.get_class()==Class::NONE);
+  EXPECT_THROW(a.super(), std::runtime_error);
+  EXPECT_THROW(a.has_class(Class::INTEGER), std::runtime_error);
+  EXPECT_THROW(a.native_type(), std::runtime_error);
+  EXPECT_THROW(a.size(), std::runtime_error);
+  EXPECT_THROW(a.set_size(1);, std::runtime_error);
 
   a = Datatype(ObjectHandle(H5Tcopy(H5T_NATIVE_INT)));
-  EXPECT_TRUE(a.get_class()==Class::INTEGER);
+  EXPECT_EQ(a.get_class(), Class::INTEGER);
+  EXPECT_EQ(a.native_type(), a);
 
   a = Datatype(ObjectHandle(H5Tcopy(H5T_NATIVE_FLOAT)));
-  EXPECT_TRUE(a.get_class()==Class::FLOAT);
+  EXPECT_EQ(a.get_class(), Class::FLOAT);
 
   a = Datatype(ObjectHandle(H5Tcreate(H5T_STRING,1)));
-  EXPECT_TRUE(a.get_class()==Class::STRING);
+  EXPECT_EQ(a.get_class(), Class::STRING);
 
   a = Datatype(ObjectHandle(H5Tcreate(H5T_COMPOUND,8)));
-  EXPECT_TRUE(a.get_class()==Class::COMPOUND);
+  EXPECT_EQ(a.get_class(), Class::COMPOUND);
   EXPECT_THROW(a.super(), std::runtime_error);
   H5Tinsert(static_cast<hid_t>(a),"hello",0,H5T_NATIVE_INT);
   EXPECT_TRUE(a.has_class(Class::INTEGER));
   EXPECT_FALSE(a.has_class(Class::FLOAT));
 
   a = Datatype(ObjectHandle(H5Tcreate(H5T_OPAQUE,1)));
-  EXPECT_TRUE(a.get_class()==Class::OPAQUE);
+  EXPECT_EQ(a.get_class(), Class::OPAQUE);
 
   a = Datatype(ObjectHandle(H5Tcreate(H5T_ENUM,1)));
-  EXPECT_TRUE(a.get_class()==Class::ENUM);
+  EXPECT_EQ(a.get_class(), Class::ENUM);
   auto b = a.super();
-  EXPECT_TRUE(b.get_class()==Class::INTEGER);
+  EXPECT_EQ(b.get_class(), Class::INTEGER);
 
+
+  a = Datatype(ObjectHandle(H5Tvlen_create(H5T_NATIVE_INT)));
+  EXPECT_EQ(a.get_class(), Class::VARLENGTH);
+
+//  a = Datatype(ObjectHandle(H5Tcopy(H5T_NATIVE_HBOOL)));
 //  a = Datatype(ObjectHandle((H5T_BITFIELD)));
 //  a = Datatype(ObjectHandle(H5Tcopy(H5T_BITFIELD)));
 //  EXPECT_TRUE(a.native_type().get_class()==Class::BITFIELD);
