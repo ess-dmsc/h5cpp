3b48a25ab549c9c55dcc5005bbcc999be20bba37
MGS <martukas@gmail.com>
2017-10-20 21:55:02 +0200

    Finally working, cleaning up Jenkinsfile #55

--- push_to_repo.sh
+++ push_to_repo.sh
@@ -2,11 +2,12 @@
 
 set username [lindex $argv 0]
 set password [lindex $argv 1]
+set wait_time [lindex $argv 2]
 
 spawn git push
 expect "Username for 'https://github.com':"
 send "$username\n"
 expect "Password for 'https://cow-bot@github.com':"
 send "$password\n"
-sleep 10
+sleep $wait_time
 interact
