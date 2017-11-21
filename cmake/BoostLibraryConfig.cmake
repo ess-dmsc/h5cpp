bb8e20d08b07a67fd020a525ea2f58bb20013db6
Martin Shetty <martukas@gmail.com>
2017-11-21 22:17:34 +0100

    fixed CMake file for examples to work independently of library, refactored some compile-related clauses for MPI and Boost, fixing #150; updates #138'

--- cmake/BoostLibraryConfig.cmake
+++ cmake/BoostLibraryConfig.cmake
@@ -4,3 +4,7 @@ set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
 
 find_package(Boost 1.41 COMPONENTS filesystem REQUIRED)
+
+if (CMAKE_CXX_COMPILER_ID MATCHES MSVC)
+  add_definitions(-DBOOST_ALL_DYN_LINK)
+endif ()
