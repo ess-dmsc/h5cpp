5ecd7bfd30d8b4283a9247eae67495e8a19cab5f
Martin Shetty <martin.shetty@esss.se>
2017-11-24 10:10:03 +0100

    some refactoring of coverage setup and ensuring of build type

--- cmake/EnsureBuildType.cmake
+++ cmake/EnsureBuildType.cmake
@@ -1,12 +1,6 @@
 if (NOT CMAKE_BUILD_TYPE)
-    message(STATUS "Build type was not set. Using Relese mode.")
+    message(STATUS "Build type was not set. Using Release mode.")
     set(CMAKE_BUILD_TYPE "Release")
-endif()
-
-#
-# On linux it is mandatory to provide a build type during configuration
-#
-if(NOT CMAKE_BUILD_TYPE)
-    message(STATUS "We only support single configuration builds!")
-    message(FATAL_ERROR "CMAKE_BUILD_TYPE must be provided during configuration")
+else ()
+  message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
 endif()
