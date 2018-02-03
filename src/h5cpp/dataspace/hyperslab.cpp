c1b3269d7a097e379683289f351e69a77b3f2ede
Martin Shetty <martukas@gmail.com>
2018-02-04 00:51:21 +0100

    more tests coverage for Dataspace; updates #213'

--- src/h5cpp/dataspace/hyperslab.cpp
+++ src/h5cpp/dataspace/hyperslab.cpp
@@ -19,7 +19,9 @@
 // Boston, MA  02110-1301 USA
 // ===========================================================================
 //
-// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Authors:
+//   Eugen Wintersberger <eugen.wintersberger@desy.de>
+//   Martin Shetty <martin.shetty@esss.se>
 // Created on: Aug 25, 2017
 //
 #include <h5cpp/dataspace/hyperslab.hpp>
@@ -34,62 +36,58 @@ namespace dataspace {
     throw std::runtime_error(msg);\
   }
 
+Hyperslab::Hyperslab() :
+    Selection(),
+    start_(),
+    stride_(),
+    count_(),
+    block_() {}
 
-Hyperslab::Hyperslab():
-        Selection(),
-        start_(),
-        stride_(),
-        count_(),
-        block_()
-{}
+Hyperslab::~Hyperslab() {}
 
-Hyperslab::~Hyperslab()
-{}
-
-Hyperslab::Hyperslab(size_t rank):
-            Selection(),
-            start_(rank),
-            stride_(rank),
-            count_(rank),
-            block_(rank)
+Hyperslab::Hyperslab(size_t rank) :
+    Selection(),
+    start_(rank),
+    stride_(rank),
+    count_(rank),
+    block_(rank)
 {
-  std::fill(start_.begin(),start_.end(),0);
-  std::fill(stride_.begin(),stride_.end(),1);
-  std::fill(block_.begin(),block_.end(),0);
-  std::fill(count_.begin(),count_.end(),0);
+  std::fill(start_.begin(), start_.end(), 0);
+  std::fill(stride_.begin(), stride_.end(), 1);
+  std::fill(block_.begin(), block_.end(), 0);
+  std::fill(count_.begin(), count_.end(), 0);
 }
 
 Hyperslab::Hyperslab(const Dimensions &offset,
                      const Dimensions &block,
                      const Dimensions &count,
-                     const Dimensions &stride):
-                         Selection(),
-                         start_(offset),
-                         stride_(stride),
-                         count_(count),
-                         block_(block)
-{}
+                     const Dimensions &stride) :
+    Selection(),
+    start_(offset),
+    stride_(stride),
+    count_(count),
+    block_(block) {}
 
-Hyperslab::Hyperslab(const Dimensions &offset,const Dimensions &block):
+Hyperslab::Hyperslab(const Dimensions &offset, const Dimensions &block) :
     Selection(),
     start_(offset),
     stride_(offset.size()),
     count_(offset.size()),
     block_(block)
 {
-  std::fill(stride_.begin(),stride_.end(),1ul);
-  std::fill(count_.begin(),count_.end(),1ul);
+  std::fill(stride_.begin(), stride_.end(), 1ul);
+  std::fill(count_.begin(), count_.end(), 1ul);
 }
 
-Hyperslab::Hyperslab(const Dimensions &offset,const Dimensions &count,
-                     const Dimensions &stride):
+Hyperslab::Hyperslab(const Dimensions &offset, const Dimensions &count,
+                     const Dimensions &stride) :
     Selection(),
     start_(offset),
     stride_(stride),
     count_(count),
     block_(offset.size())
 {
-  std::fill(block_.begin(),block_.end(),1ul);
+  std::fill(block_.begin(), block_.end(), 1ul);
 }
 
 size_t Hyperslab::rank() const noexcept
@@ -97,34 +95,11 @@ size_t Hyperslab::rank() const noexcept
   return start_.size();
 }
 
-void Hyperslab::check_dimension_index(size_t index,const std::string &what) const
-{
-  if(index>=rank())
-  {
-    std::stringstream ss;
-    ss<<"Cannot set "<<what<<" value at dimension "<<index<<" for a Hyperslab"
-        <<" of rank "<<rank()<<"!";
-    throw std::runtime_error(ss.str());
-  }
-}
-
-void Hyperslab::check_container_size(const Dimensions &container,
-                                     const std::string &what) const
-{
-  if(container.size()!=rank())
-  {
-    std::stringstream ss;
-    ss<<"Cannot assign "<<container.size()<<" "<<what<<" values to a Hyperslab"
-      <<" of rank "<<rank()<<"!";
-    throw std::runtime_error(ss.str());
-  }
-}
-
-void Hyperslab::offset(size_t index,size_t value)
+void Hyperslab::offset(size_t index, size_t value)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set start value for a default "
-                               "constructed Hyperslab!")
-  check_dimension_index(index,"start");
+                                   "constructed Hyperslab!")
+  check_dimension_index(index, "start");
 
   start_[index] = value;
 }
@@ -132,24 +107,24 @@ void Hyperslab::offset(size_t index,size_t value)
 void Hyperslab::offset(const Dimensions &values)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set start values for a default "
-                               "constructed Hyperslab!")
+                                   "constructed Hyperslab!")
 
-  check_container_size(values,"start");
-  start_  = values;
+  check_container_size(values, "start");
+  start_ = values;
 }
 
 const Dimensions &Hyperslab::offset() const
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot get start values for a default "
-                               "constructed Hyperslab!")
+                                   "constructed Hyperslab!")
   return start_;
 }
 
-void Hyperslab::stride(size_t index,size_t value)
+void Hyperslab::stride(size_t index, size_t value)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set stride value for a default "
-                               "constructed Hyperslab!")
-  check_dimension_index(index,"stride");
+                                   "constructed Hyperslab!")
+  check_dimension_index(index, "stride");
 
   stride_[index] = value;
 }
@@ -157,23 +132,23 @@ void Hyperslab::stride(size_t index,size_t value)
 void Hyperslab::stride(const Dimensions &values)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set stride values for a default "
-                               "constructed Hyperslab!")
-  check_container_size(values,"stride");
+                                   "constructed Hyperslab!")
+  check_container_size(values, "stride");
   stride_ = values;
 }
 
 const Dimensions &Hyperslab::stride() const
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot get stride values for a default "
-                               "constructed Hyperslab!")
+                                   "constructed Hyperslab!")
   return stride_;
 }
 
-void Hyperslab::count(size_t index,size_t value)
+void Hyperslab::count(size_t index, size_t value)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set count value for a default "
-                               "constructed Hyperslab!")
-  check_dimension_index(index,"count");
+                                   "constructed Hyperslab!")
+  check_dimension_index(index, "count");
 
   count_[index] = value;
 }
@@ -181,31 +156,31 @@ void Hyperslab::count(size_t index,size_t value)
 void Hyperslab::count(const Dimensions &values)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set count values for a default "
-                               "constructed Hyperslab!")
-  check_container_size(values,"count");
+                                   "constructed Hyperslab!")
+  check_container_size(values, "count");
   count_ = values;
 }
 
 const Dimensions &Hyperslab::count() const
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot get count values for a default "
-                               "constructed Hyperslab!")
+                                   "constructed Hyperslab!")
   return count_;
 }
 
-void Hyperslab::block(size_t index,size_t value)
+void Hyperslab::block(size_t index, size_t value)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set block value for a default "
-                               "constructed Hyperslab!")
-  check_dimension_index(index,"block");
+                                   "constructed Hyperslab!")
+  check_dimension_index(index, "block");
   block_[index] = value;
 }
 
 void Hyperslab::block(const Dimensions &values)
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot set block values for a default "
-                               "constructed Hyperslab!")
-  check_container_size(values,"block");
+                                   "constructed Hyperslab!")
+  check_container_size(values, "block");
   block_ = values;
 
 }
@@ -213,45 +188,43 @@ void Hyperslab::block(const Dimensions &values)
 const Dimensions &Hyperslab::block() const
 {
   THROW_IF_DEFAULT_CONSTRUCTED("Cannot get block values for a default "
-                               "constructed Hyperslab!")
+                                   "constructed Hyperslab!")
   return block_;
 }
 
-void Hyperslab::apply(const Dataspace &space,SelectionOperation ops) const
+void Hyperslab::apply(const Dataspace &space, SelectionOperation ops) const
 {
-  if(H5Sselect_hyperslab(static_cast<hid_t>(space),
-                         static_cast<H5S_seloper_t>(ops),
-                         start_.data(),stride_.data(),count_.data(),
-                         block_.data())<0)
-  {
+  if (H5Sselect_hyperslab(static_cast<hid_t>(space),
+                          static_cast<H5S_seloper_t>(ops),
+                          start_.data(), stride_.data(), count_.data(),
+                          block_.data()) < 0) {
     error::Singleton::instance().throw_with_stack("Failure applying hyperslab selection to the "
-          "dataspace");
+                                                      "dataspace");
   }
 }
 
-Dataspace operator||(const Dataspace &space,const Hyperslab &selection)
+Dataspace operator||(const Dataspace &space, const Hyperslab &selection)
 {
   Dataspace new_space(space);
 
-  new_space.selection(SelectionOperation::SET,selection);
-  std::cout<<new_space.selection.size()<<std::endl;
+  new_space.selection(SelectionOperation::SET, selection);
+  std::cout << new_space.selection.size() << std::endl;
   return new_space;
 }
 
-SelectionList operator|(const Hyperslab &a,const Hyperslab &b)
+SelectionList operator|(const Hyperslab &a, const Hyperslab &b)
 {
-  return {{SelectionOperation::SET,Selection::SharedPointer(new Hyperslab(a))},
-    {SelectionOperation::OR,Selection::SharedPointer(new Hyperslab(b))}
+  return {{SelectionOperation::SET, Selection::SharedPointer(new Hyperslab(a))},
+          {SelectionOperation::OR, Selection::SharedPointer(new Hyperslab(b))}
   };
 }
 
-SelectionList &operator|(SelectionList &selections,const Hyperslab &b)
+SelectionList &operator|(SelectionList &selections, const Hyperslab &b)
 {
   selections.push_back({SelectionOperation::SET,
-    Selection::SharedPointer(new Hyperslab(b))});
+                        Selection::SharedPointer(new Hyperslab(b))});
   return selections;
 }
-
 
 } // namespace dataspace
 } // namespace hdf5
--- src/h5cpp/dataspace/hyperslab.hpp
+++ src/h5cpp/dataspace/hyperslab.hpp
@@ -19,7 +19,9 @@
 // Boston, MA  02110-1301 USA
 // ===========================================================================
 //
-// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Authors:
+//   Eugen Wintersberger <eugen.wintersberger@desy.de>
+//   Martin Shetty <martin.shetty@esss.se>
 // Created on: Aug 25, 2017
 //
 #pragma once
@@ -48,228 +50,243 @@ namespace dataspace {
 //!
 class DLL_EXPORT Hyperslab : public Selection
 {
-  public:
-    //!
-    //! \brief default constructor
-    //!
-    //! The default constructor is present to allow Hyperslabs in STL
-    //! containers. It must be noted that any attempt to an internal value
-    //! will result in a std::runtime_error exception.
-    //! The rank of a default constructed Hyperslab would be 0. As a
-    //! Hyperslab of rank 0 would not make any sense one could use this
-    //! to identify a default constructed Hyperslab.
-    //!
-    //! \post all internall buffers remain unallocated
-    //!
-    Hyperslab();
+ public:
+  //!
+  //! \brief default constructor
+  //!
+  //! The default constructor is present to allow Hyperslabs in STL
+  //! containers. It must be noted that any attempt to an internal value
+  //! will result in a std::runtime_error exception.
+  //! The rank of a default constructed Hyperslab would be 0. As a
+  //! Hyperslab of rank 0 would not make any sense one could use this
+  //! to identify a default constructed Hyperslab.
+  //!
+  //! \post all internall buffers remain unallocated
+  //!
+  Hyperslab();
 
-    //!
-    //! \brief destructor
-    //!
-    ~Hyperslab();
+  //!
+  //! \brief destructor
+  //!
+  ~Hyperslab();
 
-    //!
-    //! \brief copy constructor
-    //!
-    //! Use compiler generated default implementation here
-    //!
-    Hyperslab(const Hyperslab &) = default;
+  //!
+  //! \brief copy constructor
+  //!
+  //! Use compiler generated default implementation here
+  //!
+  Hyperslab(const Hyperslab &) = default;
 
-    //!
-    //! \brief constructor
-    //!
-    //! Construct a Hyperslab selection of a given rank.
-    //!
-    //! \param rank the number of dimensions
-    //!
-    //! \post all start values are set to 0
-    //! \post all stride values are set to 1
-    //! \post all count values are set to 0
-    //! \post all block values are set to 0
-    //!
-    Hyperslab(size_t rank);
+  //!
+  //! \brief constructor
+  //!
+  //! Construct a Hyperslab selection of a given rank.
+  //!
+  //! \param rank the number of dimensions
+  //!
+  //! \post all start values are set to 0
+  //! \post all stride values are set to 1
+  //! \post all count values are set to 0
+  //! \post all block values are set to 0
+  //!
+  Hyperslab(size_t rank);
 
-    //!
-    //! \brief constructor
-    //!
-    //! This constructor exposes the full capabilities of a hyperslab. All
-    //! parameters can be adjusted.
-    //!
-    //! \param offset the offset from which to start the selection
-    //! \param block the size of a single block in the selection
-    //! \param count the number of blocks along each dimensions
-    //! \param stride the stride along each dimension between the blocks
-    //!
-    Hyperslab(const Dimensions &offset,
-              const Dimensions &block,
-              const Dimensions &count,
-              const Dimensions &stride);
+  //!
+  //! \brief constructor
+  //!
+  //! This constructor exposes the full capabilities of a hyperslab. All
+  //! parameters can be adjusted.
+  //!
+  //! \param offset the offset from which to start the selection
+  //! \param block the size of a single block in the selection
+  //! \param count the number of blocks along each dimensions
+  //! \param stride the stride along each dimension between the blocks
+  //!
+  Hyperslab(const Dimensions &offset,
+            const Dimensions &block,
+            const Dimensions &count,
+            const Dimensions &stride);
 
-    //!
-    //! \brief constructor
-    //!
-    //! Construct a hyperslab which constists of a single block starting
-    //! at a particular offset.
-    //!
-    //! \param offset the start offset of the block
-    //! \param block the size of the single block
-    //!
-    Hyperslab(const Dimensions &offset,
-              const Dimensions &block);
+  //!
+  //! \brief constructor
+  //!
+  //! Construct a hyperslab which constists of a single block starting
+  //! at a particular offset.
+  //!
+  //! \param offset the start offset of the block
+  //! \param block the size of the single block
+  //!
+  Hyperslab(const Dimensions &offset,
+            const Dimensions &block);
 
-    //!
-    //! \brief constructor
-    //!
-    //! Construct a hyperslab which consists of \c count blocks of size 1
-    //! along each dimension separated by \c stride elements.
-    //!
-    //! \param offset starting offset of the selection
-    //! \param count number of blocks along each dimensions
-    //! \param stride the stride between the blocks along each dimension
-    //1
-    Hyperslab(const Dimensions &offset,
-              const Dimensions &count,
-              const Dimensions &stride);
+  //!
+  //! \brief constructor
+  //!
+  //! Construct a hyperslab which consists of \c count blocks of size 1
+  //! along each dimension separated by \c stride elements.
+  //!
+  //! \param offset starting offset of the selection
+  //! \param count number of blocks along each dimensions
+  //! \param stride the stride between the blocks along each dimension
+  //1
+  Hyperslab(const Dimensions &offset,
+            const Dimensions &count,
+            const Dimensions &stride);
 
-    //!
-    //! \brief get rank
-    //! \return number of dimensions in the selection
-    //!
-    size_t rank() const noexcept;
+  //!
+  //! \brief get rank
+  //! \return number of dimensions in the selection
+  //!
+  size_t rank() const noexcept;
 
-    //!
-    //! \brief set start value for a dimension
-    //!
-    //! Set the start value of the hyperslab for a particular dimension determined
-    //! by \c index to \c value. This method throws an exception if the dimension
-    //! \c index exceeds the rank of the hyperslab.
-    //!
-    //! \throws std::runtime_error in case of a failure
-    //! \param index dimension index
-    //! \param value new start value
-    //!
-    void offset(size_t index,size_t value);
+  //!
+  //! \brief set start value for a dimension
+  //!
+  //! Set the start value of the hyperslab for a particular dimension determined
+  //! by \c index to \c value. This method throws an exception if the dimension
+  //! \c index exceeds the rank of the hyperslab.
+  //!
+  //! \throws std::runtime_error in case of a failure
+  //! \param index dimension index
+  //! \param value new start value
+  //!
+  void offset(size_t index, size_t value);
 
-    //!
-    //! \brief set all start values
-    //!
-    //! Set the start values for all dimensions of the hyperslab. If the size of
-    //! \c values exceeds the rank of the hyperslab an exception will be thrown.
-    //!
-    //! \throws std::runtime_error in case of a failure
-    //! \param values new start values for the hyperslab
-    //!
-    void offset(const Dimensions &values);
+  //!
+  //! \brief set all start values
+  //!
+  //! Set the start values for all dimensions of the hyperslab. If the size of
+  //! \c values exceeds the rank of the hyperslab an exception will be thrown.
+  //!
+  //! \throws std::runtime_error in case of a failure
+  //! \param values new start values for the hyperslab
+  //!
+  void offset(const Dimensions &values);
 
-    //!
-    //! \brief get start values
-    //! \throws std::runtime_error if the Hyperslab is default constructed
-    //! \return const reference to the start values of the hyperslab
-    //!
-    const Dimensions &offset() const;
+  //!
+  //! \brief get start values
+  //! \throws std::runtime_error if the Hyperslab is default constructed
+  //! \return const reference to the start values of the hyperslab
+  //!
+  const Dimensions &offset() const;
 
+  //!
+  //! \brief set stride value for an individual dimension
+  //!
+  //! \throws std::runtime_error in case of a failure
+  //! \param index dimension index for which to set the stride
+  //! \param value new stride value
+  //!
+  void stride(size_t index, size_t value);
 
-    //!
-    //! \brief set stride value for an individual dimension
-    //!
-    //! \throws std::runtime_error in case of a failure
-    //! \param index dimension index for which to set the stride
-    //! \param value new stride value
-    //!
-    void stride(size_t index,size_t value);
+  //!
+  //! \brief set all stride values for the Hyperslab
+  //!
+  //! \throws std::runtime_error in case of a failure
+  //! \param values new stride values for the Hyperslab
+  //!
+  void stride(const Dimensions &values);
 
-    //!
-    //! \brief set all stride values for the Hyperslab
-    //!
-    //! \throws std::runtime_error in case of a failure
-    //! \param values new stride values for the Hyperslab
-    //!
-    void stride(const Dimensions &values);
+  //!
+  //! \brief get stride values for the Hyperslab
+  //! \throws std::runtime_error in case of a failure
+  //! \return const reference to the stride values
+  //!
+  const Dimensions &stride() const;
 
-    //!
-    //! \brief get stride values for the Hyperslab
-    //! \throws std::runtime_error in case of a failure
-    //! \return const reference to the stride values
-    //!
-    const Dimensions &stride() const;
+  //!
+  //! \brief set count value for a particular dimension
+  //!
+  //! \throws std::runtime_error in case of a failure
+  //! \param index dimension index
+  //! \param value new count value for this dimension
+  //!
+  void count(size_t index, size_t value);
 
-    //!
-    //! \brief set count value for a particular dimension
-    //!
-    //! \throws std::runtime_error in case of a failure
-    //! \param index dimension index
-    //! \param value new count value for this dimension
-    //!
-    void count(size_t index,size_t value);
+  //!
+  //! \brief set all count values
+  //! \throws std::runtime_error in case of a failure
+  //! \param values reference to the new count values
+  //!
+  void count(const Dimensions &values);
 
-    //!
-    //! \brief set all count values
-    //! \throws std::runtime_error in case of a failure
-    //! \param values reference to the new count values
-    //!
-    void count(const Dimensions &values);
+  //!
+  //! \brief get count values
+  //!
+  //! Return a const reference to the count values of the Hypeslab. The
+  //! values cannot be changed via this reference.
+  //!
+  //! \throws std::runtime_error in case of a failure
+  //! \return reference to count values
+  //!
+  const Dimensions &count() const;
 
-    //!
-    //! \brief get count values
-    //!
-    //! Return a const reference to the count values of the Hypeslab. The
-    //! values cannot be changed via this reference.
-    //!
-    //! \throws std::runtime_error in case of a failure
-    //! \return reference to count values
-    //!
-    const Dimensions &count() const;
+  //!
+  //! \brief set block size for dimension
+  //!
+  //! \throws std::runtime_error in case of a failure
+  //! \param index dimension index
+  //! \param value the new block size for this dimension
+  //1
+  void block(size_t index, size_t value);
 
-    //!
-    //! \brief set block size for dimension
-    //!
-    //! \throws std::runtime_error in case of a failure
-    //! \param index dimension index
-    //! \param value the new block size for this dimension
-    //1
-    void block(size_t index,size_t value);
+  //!
+  //! \brief set all block values
+  //!
+  //! Set all block values for the hyperslab selection.
+  //! \throws std::runtime_error in case of a failure
+  //! \param values reference to the new block values
+  //!
+  void block(const Dimensions &values);
 
-    //!
-    //! \brief set all block values
-    //!
-    //! Set all block values for the hyperslab selection.
-    //! \throws std::runtime_error in case of a failure
-    //! \param values reference to the new block values
-    //!
-    void block(const Dimensions &values);
+  //!
+  //! \brief get block values
+  //!
+  //! Get a const reference to the block values of the hyperslab.
+  //! \throws std::runtime_error in case of a failure
+  //! \return const reference to the block values
+  //!
+  const Dimensions &block() const;
 
-    //!
-    //! \brief get block values
-    //!
-    //! Get a const reference to the block values of the hyperslab.
-    //! \throws std::runtime_error in case of a failure
-    //! \return const reference to the block values
-    //!
-    const Dimensions &block() const;
+  virtual void apply(const Dataspace &space,
+                     SelectionOperation ops) const;
 
-    virtual void apply(const Dataspace &space,
-                       SelectionOperation ops) const;
+ private:
+  inline void check_dimension_index(size_t index, const std::string &what) const
+  {
+    if (index >= rank()) {
+      std::stringstream ss;
+      ss << "Cannot set " << what << " value at dimension " << index << " for a Hyperslab"
+         << " of rank " << rank() << "!";
+      throw std::runtime_error(ss.str());
+    }
+  }
 
-  private:
-    void check_dimension_index(size_t index,const std::string &what) const;
-    void check_container_size(const Dimensions &container,const std::string &what) const;
+  inline void check_container_size(const Dimensions &container, const std::string &what) const
+  {
+    if (container.size() != rank()) {
+      std::stringstream ss;
+      ss << "Cannot assign " << container.size() << " " << what << " values to a Hyperslab"
+         << " of rank " << rank() << "!";
+      throw std::runtime_error(ss.str());
+    }
+  }
 
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable:4251)
 #endif
-    Dimensions start_;
-    Dimensions stride_;
-    Dimensions count_;
-    Dimensions block_;
+  Dimensions start_;
+  Dimensions stride_;
+  Dimensions count_;
+  Dimensions block_;
 #ifdef _MSC_VER
 #pragma warning(pop)
 #endif
 
 };
 
-DLL_EXPORT Dataspace operator||(const Dataspace &space,const Hyperslab &selection);
-
+DLL_EXPORT Dataspace operator||(const Dataspace &space, const Hyperslab &selection);
 
 } // namespace dataspace
 } // namespace hdf5
