f331dd18bb94b8ae9266e7bac73f4c8f0e3073b8
Jan Kotanski <jankotan@gmail.com>
2018-07-05 20:45:19 +0200

    update data test files

--- test/create_h5py_testboolattr.py
+++ test/create_h5py_testboolattr.py
@@ -7,4 +7,9 @@ f.attrs["bool_scalar_false"] = False
 f.attrs["bool_simple_true"] = [True]
 f.attrs["bool_simple_false"] = [False]
 f.attrs["bool_array"] = [False, True, True, False]
+f.create_dataset("ds_bool_scalar_true",data=True)
+f.create_dataset("ds_bool_scalar_false",data=False)
+f.create_dataset("ds_bool_simple_true",data=[True])
+f.create_dataset("ds_bool_simple_false",data=[False])
+f.create_dataset("ds_bool_array", data=[False, True, True, False])
 f.close()
--- test/create_pniio_testboolattr.py
+++ test/create_pniio_testboolattr.py
@@ -6,4 +6,8 @@ rt.attributes.create("bool_true", "bool")[...] = True
 rt.attributes.create("bool_false", "bool")[...] = False
 rt.attributes.create("bool_array", "bool", shape=[4])[...] \
     = [False, True, True, False]
+rt.create_field("ds_bool_scalar_false", "bool")[...] = False
+rt.create_field("ds_bool_scalar_true", "bool")[...] = True
+rt.create_field("ds_bool_array", "bool", [4], [4])[...] \
+    = [False, True, True, False]
 fl.close()
