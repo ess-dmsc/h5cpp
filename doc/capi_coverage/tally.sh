8a30a19ca0b9294cfc8462aec4e0bd1b37ad0923
Martin Shetty <martukas@gmail.com>
2018-07-08 02:52:46 +0200

    script constructs docs; updates #306'

--- doc/capi_coverage/tally.sh
+++ doc/capi_coverage/tally.sh
@@ -3,7 +3,6 @@
 SOURCE_PATH=$1
 RST_PATH=$2
 
-OUTPUT_DIR=$PWD/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LISTS_PATH=$DIR/functions/*
@@ -11,33 +10,37 @@ LISTS_PATH=$DIR/functions/*
 tot_found=0
 tot_not_found=0
 
-rstfile=${RST_PATH}/capi.rst
+rstfile=${RST_PATH}/index.rst
 
-text=$(printf '===============\nC API Coverage\n===============\n')
-echo "${text}" > $rstfile
+echo "==================" > $rstfile
+echo "C API Coverage" >> $rstfile
+echo "==================" >> $rstfile
+echo "" >> $rstfile
+echo ".. csv-table:: Wrapped C API functions" >> $rstfile
+echo '   :header: "API", "wrapped", "total", "percent"' >> $rstfile
+echo "   :widths: 20, 20, 20, 20" >> $rstfile
+echo "" >> $rstfile
 
-text=$(printf '\n')
-echo "${text}" >> $rstfile
-
-text=$(printf '.. csv-table:: Wrapped C API functions\n   :header: "API", "wrapped", "total", "percent"\n   :widths: 20, 20, 20, 20\n')
-echo "${text}" >> $rstfile
-
-text=$(printf '\n')
-echo "${text}" >> $rstfile
 
 for filename in ${LISTS_PATH}; do
   basename=$(basename $filename)
-  outfile=${OUTPUT_DIR}/${basename}
+  outfile=${RST_PATH}/${basename}.rst
   found=0
   not_found=0
 
-  echo "" > $outfile
+  echo ".. _capi-${basename}:" > $outfile
+  echo "" >> $outfile
+  echo "===================================" >> $outfile
+  echo "Wrapper coverage of ${basename} functions" >> $outfile
+  echo "===================================" >> $outfile
+  echo "" >> $outfile
 
   while read line; do
     if grep -qr "$line" ${SOURCE_PATH}; then
+      echo -e "* **$line**\n" >> $outfile
       ((found++))
     else
-      echo "$line" >> $outfile
+      echo -e "* $line\n" >> $outfile
       ((not_found++))
     fi
   done <<< "$(cat ${filename})"
@@ -48,10 +51,9 @@ for filename in ${LISTS_PATH}; do
 
   percent=$(awk "BEGIN { pc=100*${found}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 
-  text=$(printf '   "%s", "%s", "%s", "%s"\n' "$basename" "$found" "$total" "$percent%" | expand -t 7)
+  text=$(printf '   ":ref:`%s <capi-%s>`", "%s", "%s", "%s"\n' "$basename" "$basename" "$found" "$total" "$percent%" | expand -t 7)
 
   echo "" >> $outfile
-  echo "${text}" >> $outfile
   echo "${text}" >> $rstfile
 
 done
