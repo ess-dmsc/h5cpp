e205ece44513b0ecf8bb17e383c84e979c399352
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2018-12-11 21:30:51 +0100

    Fixed conan executable search for IDEs
    Update #302

--- cmake/ConanSetup.cmake
+++ cmake/ConanSetup.cmake
@@ -1,11 +1,12 @@
-find_program(CONAN NAMES conan PATHS ${CONAN_PATH})
-if(CONAN MATCHES CONAN-NOTFOUND)
+find_program(CONAN_EXEC NAMES conan PATHS ${CONAN_PATH})
+if(CONAN_EXEC MATCHES CONAN_EXEC-NOTFOUND)
     message(FATAL_ERROR "Could not find conan executable to install dependencies ${CONAN_PATH}")
 else()
-    message(STATUS "Found conan: ${CONAN}")
+    message(STATUS "Found conan: ${CONAN_EXEC}")
     
     #need to add the directory to the path
-    get_filename_component(CONAN_EXEC_PATH ${CONAN} DIRECTORY)
+    get_filename_component(CONAN_EXEC_PATH ${CONAN_EXEC} DIRECTORY)
+    message(STATUS "Conan executable found in: ${CONAN_EXEC_PATH}")
     set(ENV{PATH} "$ENV{PATH}:${CONAN_EXEC_PATH}")
 endif()
 
