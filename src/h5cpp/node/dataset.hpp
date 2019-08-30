460789b7f2f138ffe7448395cdb7ea071ee690f7
Jan Kotanski <jankotan@gmail.com>
2019-08-30 17:11:36 +0200

    add checks if dataspace is the same in write() method

--- src/h5cpp/node/dataset.hpp
+++ src/h5cpp/node/dataset.hpp
@@ -727,12 +727,29 @@ void Dataset::write(const T &data,const dataspace::Selection &selection,
 {
   auto memory_space = hdf5::dataspace::create(data);
   auto memory_type  = hdf5::datatype::create(data);
-
+  
   dataspace::Dataspace file_space = dataspace();
   file_space.selection(dataspace::SelectionOperation::SET,selection);
-  write(data,memory_type,memory_space,file_space,dtpl);
-}
+  try{
 
+    const dataspace::Hyperslab & hyper = dynamic_cast<const dataspace::Hyperslab &>(selection);
+
+    auto dims = hyper.block();
+    auto count = hyper.count();
+    for(Dimensions::size_type i = 0; i != dims.size(); i++)
+      dims[i] *= count[i];
+    dataspace::Simple selected_space(dims);
+    if(selected_space.rank() > 1 &&
+       selected_space.size() == memory_space.size())
+      write(data,memory_type,selected_space,file_space,dtpl);
+    else
+      write(data,memory_type,memory_space,file_space,dtpl);
+      
+  }
+  catch(const std::bad_cast&){
+    write(data,memory_type,memory_space,file_space,dtpl);
+  }
+}
 
 
 template<typename T>
