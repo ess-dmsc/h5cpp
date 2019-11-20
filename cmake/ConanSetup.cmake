1dab3a57a8eb318047f48895683c68c64b02dc7f
Jan Kotanski <jankotan@gmail.com>
2019-11-20 20:15:30 +0100

    create pkgconfig file only without CONAN

--- cmake/ConanSetup.cmake
+++ cmake/ConanSetup.cmake
@@ -3,7 +3,7 @@ if(CONAN_EXEC MATCHES CONAN_EXEC-NOTFOUND)
     message(FATAL_ERROR "Could not find conan executable to install dependencies ${CONAN_PATH}")
 else()
     message(STATUS "Found conan: ${CONAN_EXEC}")
-    
+
     #need to add the directory to the path
     get_filename_component(CONAN_EXEC_PATH ${CONAN_EXEC} DIRECTORY)
     message(STATUS "Conan executable found in: ${CONAN_EXEC_PATH}")
