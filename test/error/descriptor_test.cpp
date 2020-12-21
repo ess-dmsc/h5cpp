edf575bcf24d136eb4c4582ddb3c140bcfca7b27
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2020-12-21 19:42:56 +0100

    Updated all error tests to catch2
    Update #445

--- test/error/descriptor_test.cpp
+++ test/error/descriptor_test.cpp
@@ -1,5 +1,6 @@
 //
 // (c) Copyright 2017 DESY,ESS
+//               2020 Eugen Wintersberger <eugen.wintersberger@gmail.com>
 //
 // This file is part of h5pp.
 //
@@ -20,63 +21,71 @@
 // ===========================================================================
 //
 // Author: Martin Shetty <martin.shetty@esss.se>
+//         Eugen Wintersberger <eugen.wintersberger@gmail.com>
 // Created on: May 5, 2018
 //
-
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include <sstream>
+#include <catch2/catch.hpp>
 #include "../h5cpp_test_helpers.hpp"
+#include "../utilities.hpp"
 
 using namespace hdf5;
 
-TEST(Descriptor, extract_data)
-{
+SCENARIO("extracting error data") {
   provoke_h5_error();
-  auto stack = error::Singleton::instance().extract_stack();
-  EXPECT_GT(stack.contents().size(), 0u);
-  auto d = stack.contents().front();
-
-  EXPECT_GT(d.line, 10u);
-  EXPECT_EQ(d.function, "H5Iget_ref");
-  EXPECT_NE(d.file.find("H5I.c"), std::string::npos);
-  EXPECT_EQ(d.description, "can't get ID ref count");
+  GIVEN("an error stack instance") {
+    auto stack = error::Singleton::instance().extract_stack();
+    THEN("there must be some content in the stack") {
+      REQUIRE(stack.contents().size() > 0u);
+      WHEN("we can extract this content") {
+        auto d = stack.contents().front();
+        THEN("we get") {
+          REQUIRE(d.line > 10u);
+          REQUIRE(d.function == "H5Iget_ref");
+          REQUIRE(d.file.find("H5I.c") != std::string::npos);
+          REQUIRE(d.description == "can't get ID ref count");
+        }
+        THEN("we can extract the error strings") {
+          d.extract_strings();
+          REQUIRE(d.major == "Object atom");
+          REQUIRE(d.minor == "Can't get value");
+        }
+      }
+    }
+  }
 }
 
-TEST(Descriptor, extract_strings)
-{
-  provoke_h5_error();
-  auto stack = error::Singleton::instance().extract_stack();
-  EXPECT_GT(stack.contents().size(), 0u);
-  auto d = stack.contents().front();
-
-  d.extract_strings();
-  EXPECT_EQ(d.major, "Object atom");
-  EXPECT_EQ(d.minor, "Can't get value");
-}
-
-TEST(Descriptor, stream)
-{
-  hdf5::ObjectHandle invalid_handle;
-  H5Iget_ref(static_cast<hid_t>(invalid_handle));
-  auto d = error::Singleton::instance().extract_stack().contents().front();
-
+SCENARIO("testing error stream output") {
   std::stringstream ss;
-  ss << d;
-  EXPECT_NE(ss.str().find(d.description), std::string::npos);
+  GIVEN("an invalid object handle") {
+    hdf5::ObjectHandle invalid_handle;
+    WHEN("we try to get the reference from the handle") {
+      H5Iget_ref(to_hid(invalid_handle));
+      THEN("we provoke an error") {
+        auto d =
+            error::Singleton::instance().extract_stack().contents().front();
+        AND_THEN("we can write this error stack to a stream") {
+          ss << d;
+          REQUIRE(ss.str().find(d.description) != std::string::npos);
+        }
+      }
+    }
+  }
 }
 
 // To ensure no-throw, C-string pointers must be null-initialized
 // Talk to HDF5 group to see if they could make this happen
-TEST(Descriptor, illegal)
-{
+SCENARIO("testing illegal pointer initialization for error descriptors") { 
   H5E_error2_t descr;
   descr.file_name = nullptr;
   descr.func_name = nullptr;
   descr.desc = nullptr;
-  EXPECT_NO_THROW(error::Descriptor f(descr));
+  REQUIRE_NOTHROW(error::Descriptor{descr});
   error::Descriptor f(descr);
 
   // This also generates errors on the error stack!!! Insane!
-  EXPECT_NO_THROW(f.extract_strings());
+  REQUIRE_NOTHROW(f.extract_strings());
 
   error::Singleton::instance().clear_stack();
 }
