6b9f86b10f4661c9b0fe924910577365fe76b7e1
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2021-03-13 12:37:36 +0100

    Intermediate update
    Update #445

--- test/node/dataset_read_speed_test.cpp
+++ test/node/dataset_read_speed_test.cpp
@@ -1,5 +1,6 @@
 //
 // (c) Copyright 2017 DESY,ESS
+//               2021 Eugen Wintersberger <eugen.wintersberger@gmail.com>
 //
 // This file is part of h5pp.
 //
@@ -20,152 +21,152 @@
 // ===========================================================================
 //
 // Author: Jan Kotanski <jan.kotanski@desy.de>
+//         Eugen Wintersberger <eugen.wintersberger@gmail.com>
 // Created on: Nov 12, 2018
 //
 
-#include <gtest/gtest.h>
+#include <catch2/catch.hpp>
+#include <h5cpp/core/filesystem.hpp>
+#include <h5cpp/dataspace/hyperslab.hpp>
 #include <h5cpp/file/functions.hpp>
 #include <h5cpp/node/group.hpp>
-#include <h5cpp/dataspace/hyperslab.hpp>
-#include <h5cpp/core/filesystem.hpp>
 #ifndef _MSC_VER
 #include <sys/time.h>
 #endif
 
 using namespace hdf5;
 
-class DatasetReadSpeedTest : public testing::Test
-{
- protected:
-  virtual void SetUp()
-  {
-#if H5_VERSION_GE(1, 10, 0)
-    property::FileCreationList fcpl;
-    property::FileAccessList fapl;
-    fapl.library_version_bounds(property::LibVersion::LATEST,
-                                property::LibVersion::LATEST);
-    file::File f = file::create("dataset_read_speed.h5", file::AccessFlags::TRUNCATE, fcpl, fapl);
-#else
-    file::File f = file::create("dataset_read_speed.h5", file::AccessFlags::TRUNCATE);
-#endif
-    property::DatasetCreationList dcpl;
-    property::LinkCreationList lcpl;
-    property::DatasetAccessList dapl;
-    node::Group root = f.root();
-    long long unsigned int xdim = 867;
-    long long unsigned int ydim = 700;
-    long long unsigned int nframe = 33;
-
-     dataspace::Simple space {{0, xdim, ydim}, {dataspace::Simple::UNLIMITED,
-	   dataspace::Simple::UNLIMITED,
-	   dataspace::Simple::UNLIMITED}};
-    dcpl.layout(property::DatasetLayout::CHUNKED);
-    dcpl.chunk({1, xdim, ydim});
-    node::Dataset data = node::Dataset(root,
-				       "data",
-				       datatype::create<unsigned short int>(),
-				       space,lcpl,dcpl,dapl);
-    std::vector<unsigned short int> frame(xdim*ydim);
-    dataspace::Hyperslab framespace{{0, 0, 0}, {1, xdim, ydim}};
-    for(long long unsigned int i = 0; i != nframe; i++){
-      data.extent(0, 1);
-      framespace.offset({i, 0, 0});
-      data.write(frame, framespace);
-
-    }
-  }
-  virtual void TearDown() {
-    fs::remove("dataset_read_speed.h5");
-  }
-};
-
 #ifndef _MSC_VER
-TEST_F(DatasetReadSpeedTest, read)
-{
-  struct timeval stime1;
-  struct timeval etime1;
-  struct timeval stime0;
-  struct timeval etime0;
 
-  file::File f0 = file::open("dataset_read_speed.h5",
-			    file::AccessFlags::READONLY);
-  auto root0 = f0.root();
-  auto dataset0 = root0.get_dataset("/data");
-  hdf5::dataspace::Simple dataspace(dataset0.dataspace());
-  const auto dims = dataspace.current_dimensions();
-  std::vector<unsigned short int> buffer(dims[0]*dims[1]*dims[2]);
-  auto datatype = dataset0.datatype();
+const static long long unsigned int xdim = 867;
+const static long long unsigned int ydim = 700;
+const static long long unsigned int nframe = 33;
 
-  hdf5::Dimensions frameoffset{0, 0, 0};
-  hdf5::Dimensions frameblock{dims[0], dims[1], dims[2]};
-  hdf5::dataspace::Hyperslab selected_frames{frameoffset, frameblock};
-  // time0
-  gettimeofday(&stime0, NULL);
-  dataset0.read(buffer, datatype, dataspace, selected_frames);
-  gettimeofday(&etime0, NULL);
-  f0.close();
-
-  file::File f1 = file::open("dataset_read_speed.h5",
-			    file::AccessFlags::READONLY);
-  auto root1 = f1.root();
-  auto dataset1 = root1.get_dataset("/data");
-  // time1
-  gettimeofday(&stime1, NULL);
-  dataset1.read(buffer);
-  gettimeofday(&etime1, NULL);
-  f1.close();
-
-  double time0 = (double)(etime0.tv_sec - stime0.tv_sec)
-    + (double)(etime0.tv_usec - stime0.tv_usec)*0.000001;
-  double time1 = (double)(etime1.tv_sec - stime1.tv_sec)
-    + (double)(etime1.tv_usec - stime1.tv_usec)*0.000001;
-
-  EXPECT_GT(14 * time1, time0);
-  EXPECT_GT(14 * time0, time1);
+double delta_time(const struct timeval& start, const struct timeval& stop) {
+  return (double)(stop.tv_sec - start.tv_sec) +
+         (double)(stop.tv_usec - start.tv_usec) * 0.000001;
 }
 
-TEST_F(DatasetReadSpeedTest, read_hyperslab)
-{
-  struct timeval stime1;
-  struct timeval etime1;
-  struct timeval stime0;
-  struct timeval etime0;
+hdf5::Dimensions current_dimensions(const hdf5::node::Dataset& dataset) {
+  hdf5::dataspace::Simple dataspace(dataset.dataspace());
+  return dataspace.current_dimensions();
+}
 
-  file::File f0 = file::open("dataset_read_speed.h5",
-			    file::AccessFlags::READONLY);
-  auto root0 = f0.root();
-  auto dataset0 = root0.get_dataset("/data");
-  hdf5::dataspace::Simple dataspace(dataset0.dataspace());
-  const auto dims = dataspace.current_dimensions();
-  std::vector<unsigned short int> buffer(11*dims[1]*dims[2]);
-  auto datatype = dataset0.datatype();
+using UShorts = std::vector<unsigned short int>;
 
-  hdf5::Dimensions frameoffset{10, 0, 0};
-  hdf5::Dimensions frameblock{11, dims[1], dims[2]};
-  hdf5::dataspace::Hyperslab selected_frames{frameoffset, frameblock};
-  dataspace::Simple dataspace0(Dimensions({11, dims[1], dims[2]}));
-  // time0
-  gettimeofday(&stime0, NULL);
-  dataset0.read(buffer, datatype, dataspace0, selected_frames);
-  gettimeofday(&etime0, NULL);
-  f0.close();
+SCENARIO("testing read performance") {
+#if H5_VERSION_GE(1, 10, 0)
+  property::FileCreationList fcpl;
+  property::FileAccessList fapl;
+  fapl.library_version_bounds(property::LibVersion::LATEST,
+                              property::LibVersion::LATEST);
+  file::File f = file::create("dataset_read_speed.h5",
+                              file::AccessFlags::TRUNCATE, fcpl, fapl);
+#else
+  file::File f =
+      file::create("dataset_read_speed.h5", file::AccessFlags::TRUNCATE);
+#endif
+  property::DatasetCreationList dcpl;
+  property::LinkCreationList lcpl;
+  property::DatasetAccessList dapl;
+  auto root = f.root();
 
-  file::File f1 = file::open("dataset_read_speed.h5",
-  			    file::AccessFlags::READONLY);
-  auto root1 = f1.root();
-  auto dataset1 = root1.get_dataset("/data");
-  // time1
-  gettimeofday(&stime1, NULL);
-  dataset1.read(buffer, selected_frames);
-  gettimeofday(&etime1, NULL);
-  f1.close();
+  dataspace::Simple space{
+      {0, xdim, ydim},
+      {dataspace::Simple::UNLIMITED, dataspace::Simple::UNLIMITED,
+       dataspace::Simple::UNLIMITED}};
+  dcpl.layout(property::DatasetLayout::CHUNKED);
+  dcpl.chunk({1, xdim, ydim});
+  node::Dataset data(root, "data", datatype::create<unsigned short int>(),
+                     space, lcpl, dcpl, dapl);
+  UShorts frame(xdim * ydim);
+  dataspace::Hyperslab framespace{{0, 0, 0}, {1, xdim, ydim}};
+  for (long long unsigned int i = 0; i != nframe; i++) {
+    data.extent(0, 1);
+    framespace.offset({i, 0, 0});
+    data.write(frame, framespace);
+  }
 
-  double time0 = (double)(etime0.tv_sec - stime0.tv_sec)
-    + (double)(etime0.tv_usec - stime0.tv_usec)*0.000001;
-  double time1 = (double)(etime1.tv_sec - stime1.tv_sec)
-    + (double)(etime1.tv_usec - stime1.tv_usec)*0.000001;
+  GIVEN("the timepoints") {
+    double time0, time1;
+    AND_GIVEN("a new datafile") {
+      auto f0 =
+          file::open("dataset_read_speed.h5", file::AccessFlags::READONLY);
+      auto r0 = f0.root();
+      auto d0 = r0.get_dataset("/data");
+      auto dims = current_dimensions(d0);
+      auto dtype = d0.datatype();
+      UShorts buffer(dims[0] * dims[1] * dims[2]);
 
-  EXPECT_GT(14 * time1, time0);
-  EXPECT_GT(14 * time0, time1);
+      WHEN("selecting the entire dataset block") {
+        hdf5::Dimensions frameoffset{0, 0, 0};
+        hdf5::Dimensions frameblock{dims[0], dims[1], dims[2]};
+        hdf5::dataspace::Hyperslab selected_frames{frameoffset, frameblock};
+        dataspace::Simple dspace = d0.dataspace();
+        struct timeval start, stop;
+        gettimeofday(&start, NULL);
+        d0.read(buffer, dtype, dspace, selected_frames);
+        gettimeofday(&stop, NULL);
+        time0 = delta_time(start,stop);
+      }
+      WHEN("reading the entire dataset") {
+        struct timeval start, stop;
+        gettimeofday(&start, NULL);
+        d0.read(buffer);
+        gettimeofday(&stop, NULL);
+        time1 = delta_time(start, stop);
+      }
+    }
+    THEN("we expect") {
+      UNSCOPED_INFO("reading with selection "<<time0);
+      UNSCOPED_INFO("reading without selection "<<time1);
+      REQUIRE(14 * time1 > time0);
+      REQUIRE(14 * time0 > time1);
+    }
+  }
+
+  /*
+  TEST_F(DatasetReadSpeedTest, read_hyperslab) {
+    struct timeval stime1;
+    struct timeval etime1;
+    struct timeval stime0;
+    struct timeval etime0;
+
+    file::File f0 =
+        file::open("dataset_read_speed.h5", file::AccessFlags::READONLY);
+    auto root0 = f0.root();
+    auto dataset0 = root0.get_dataset("/data");
+    hdf5::dataspace::Simple dataspace(dataset0.dataspace());
+    const auto dims = dataspace.current_dimensions();
+    std::vector<unsigned short int> buffer(11 * dims[1] * dims[2]);
+    auto datatype = dataset0.datatype();
+
+    hdf5::Dimensions frameoffset{10, 0, 0};
+    hdf5::Dimensions frameblock{11, dims[1], dims[2]};
+    hdf5::dataspace::Hyperslab selected_frames{frameoffset, frameblock};
+    dataspace::Simple dataspace0(Dimensions({11, dims[1], dims[2]}));
+    // time0
+    gettimeofday(&stime0, NULL);
+    dataset0.read(buffer, datatype, dataspace0, selected_frames);
+    gettimeofday(&etime0, NULL);
+    f0.close();
+
+    file::File f1 =
+        file::open("dataset_read_speed.h5", file::AccessFlags::READONLY);
+    auto root1 = f1.root();
+    auto dataset1 = root1.get_dataset("/data");
+    // time1
+    gettimeofday(&stime1, NULL);
+    dataset1.read(buffer, selected_frames);
+    gettimeofday(&etime1, NULL);
+    f1.close();
+
+    double time0 = delta_time(stime0, etime0);
+    double time1 = delta_time(stime1, etime1);
+
+    EXPECT_GT(14 * time1, time0);
+    EXPECT_GT(14 * time0, time1);
+  }
+  */
 }
 #endif
