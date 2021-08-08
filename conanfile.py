bc749ef8e44fed59c3f4a15e3eb1e43786e020ca
Andrew Marshall <planetmarshalluk@gmail.com>
2021-08-08 18:27:06 +0100

    [ci] use conan to build using cmake

--- conanfile.py
+++ conanfile.py
@@ -3,16 +3,23 @@ from conans import ConanFile, CMake
 
 class H5CppConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "gcc", "txt"
+    generators = "cmake", "cmake_find_package"
     options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
         "with_mpi": [True, False],
         "with_boost": [True, False]
     }
     default_options = {
+        "shared": False,
+        "fPIC": True,
         "with_mpi": False,
         "with_boost": True,
         "hdf5:enable_cxx": False
     }
+
+    def build_requirements(self):
+        self.build_requires("gtest/1.11.0")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -26,10 +33,15 @@ class H5CppConan(ConanFile):
         self.requires("hdf5/1.12.0")
         if self.options.with_boost:
             self.requires("boost/1.76.0")
-        if self.options.with_mpi:
+        if self.options.get_safe("with_mpi", False):
             self.requires("openmpi/4.1.0")
 
     def build(self):
-        cmake = CMake(self)
+        cmake = CMake(self, generator="Ninja")
+        cmake.definitions.update({
+            "CONAN": "MANUAL",
+            "WITH_MPI": self.options.get_safe("with_mpi", False),
+            "WITH_BOOST": self.options.with_boost
+        })
         cmake.configure()
         cmake.build()
