ca5cda30cf652a9472d7063bcfeeff4136b380cb
Andrew Marshall <planetmarshalluk@gmail.com>
2021-08-10 13:47:49 +0100

    [cmake] prefix h5cpp status messages

--- cmake/MPIConfig.cmake
+++ cmake/MPIConfig.cmake
@@ -2,11 +2,11 @@ option(H5CPP_WITH_MPI "Enable MPI support" OFF)
 
 if (H5CPP_WITH_MPI)
   if (CMAKE_SYSTEM_NAME MATCHES Windows)
-    message(FATAL_ERROR "MPI not supported on the Windows platform!")
+    h5cpp_message(FATAL_ERROR "MPI not supported on the Windows platform!")
   endif ()
 
   if (CMAKE_SYSTEM_NAME MATCHES Darwin)
-    message(FATAL_ERROR "MPI not supported for OSX")
+    h5cpp_message(FATAL_ERROR "MPI not supported for OSX")
   endif ()
 
   if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
@@ -14,7 +14,7 @@ if (H5CPP_WITH_MPI)
   endif()
 
   find_package(MPI REQUIRED)
-  message(STATUS "Found MPI headers in: ${MPI_C_INCLUDE_PATH}")
-  message(STATUS "Found MPI libraries: ${MPI_C_LIBRARIES}")
+  h5cpp_message(STATUS "Found MPI headers in: ${MPI_C_INCLUDE_PATH}")
+  h5cpp_message(STATUS "Found MPI libraries: ${MPI_C_LIBRARIES}")
 
 endif ()
