7e4bbbfc7ad594444644966a41802b581399f4a0
Eugen Wintersberger <wintersberger@unisoftwareplus.com>
2021-08-15 22:39:42 +0200

    somehow test are back on windows
    Update #445

--- cmake/OutputDirConfig.cmake
+++ cmake/OutputDirConfig.cmake
@@ -8,6 +8,8 @@
 # This should be the same on all platforms just to avoid confusion
 # =============================================================================
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
 
