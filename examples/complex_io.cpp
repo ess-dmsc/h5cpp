b4e4badf2e7865b0735c368dce284132deded23b
Andrew Marshall <planetmarshalluk@gmail.com>
2022-01-30 17:11:16 +0000

    Revert "[examples] remove complex_io.cpp (linking error)"
    This reverts commit 23cd78ca5828e1f347a205365ded52d94cbb9e98.

---
+++ examples/complex_io.cpp
@@ -0,0 +1,58 @@
+//
+// (c) Copyright 2017 DESY,ESS
+//
+// This file is part of h5cpp.
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor
+// Boston, MA  02110-1301 USA
+// ===========================================================================
+//
+// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Created on: Oct 07, 2017
+//
+#include <h5cpp/hdf5.hpp>
+#include <iostream>
+#include <complex>
+
+using namespace hdf5;
+
+using ComplexDouble = std::complex<double>;
+
+int main()
+{
+  file::File f = file::create("writing_complex.h5",file::AccessFlags::Truncate);
+  node::Group root_group = f.root();
+  node::Dataset dataset(root_group,"data",datatype::create<ComplexDouble>(),
+                                                    dataspace::Scalar());
+
+  //
+  // writing a complex number
+  //
+  ComplexDouble write(1.2,-3.4231);
+  std::cout<<"writing: "<<write<<std::endl;
+  dataset.write(write);
+
+  //
+  // reading a complex number
+  //
+  ComplexDouble read(0,0);
+  dataset.read(read);
+
+  std::cout<<"reading: "<<read<<std::endl;
+
+  return 0;
+}
+
+
