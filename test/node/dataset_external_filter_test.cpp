ee265f2a8b24986b5eb464f2e177d1d1a5d7d174
Yuelong Yu <yuelong.yu@desy.de>
2022-10-19 09:40:24 +0200

    Remove test for writing data with external filter

--- test/node/dataset_external_filter_test.cpp
+++ test/node/dataset_external_filter_test.cpp
@@ -29,67 +29,66 @@
 
 using namespace hdf5;
 
+// SCENARIO("writing dataset with external filter blosclz") {
+// #if H5_VERSION_GE(1, 10, 0)
+//   property::FileCreationList fcpl;
+//   property::FileAccessList fapl;
+//   fapl.library_version_bounds(property::LibVersion::Latest,
+//                               property::LibVersion::Latest);
+//   auto f = file::create("dataset_external_filter.h5", file::AccessFlags::Truncate,
+//                         fcpl, fapl);
+// #else
+//   auto f = file::create("dataset_external_filter.h5", file::AccessFlags::Truncate);
+// #endif
+//   auto root = f.root();
 
-SCENARIO("writing dataset with external filter blosclz") {
-#if H5_VERSION_GE(1, 10, 0)
-  property::FileCreationList fcpl;
-  property::FileAccessList fapl;
-  fapl.library_version_bounds(property::LibVersion::Latest,
-                              property::LibVersion::Latest);
-  auto f = file::create("dataset_external_filter.h5", file::AccessFlags::Truncate,
-                        fcpl, fapl);
-#else
-  auto f = file::create("dataset_external_filter.h5", file::AccessFlags::Truncate);
-#endif
-  auto root = f.root();
+//   const long long unsigned int xdim = 867;
+//   const long long unsigned int ydim = 700;
+//   const long long unsigned int nframe = 33;
 
-  const long long unsigned int xdim = 867;
-  const long long unsigned int ydim = 700;
-  const long long unsigned int nframe = 33;
+//   // external filter
+//   const unsigned int FILTER_BLOSC=32001;
+//   std::vector<unsigned int> cd_values(7,0);
+//   /* 0 to 3 (inclusive) param slots are reserved. */
+//   cd_values[4] = 4;       /* compression level */
+//   cd_values[5] = 0;       /* 0: shuffle not active, 1: shuffle active */
+//   cd_values[6] = 0; /* the actual compressor to use, 0: BLOSC_BLOSCLZ */
 
-  // external filter
-  const unsigned int FILTER_BLOSC=32001;
-  std::vector<unsigned int> cd_values(7,0);
-  /* 0 to 3 (inclusive) param slots are reserved. */
-  cd_values[4] = 4;       /* compression level */
-  cd_values[5] = 0;       /* 0: shuffle not active, 1: shuffle active */
-  cd_values[6] = 0; /* the actual compressor to use, 0: BLOSC_BLOSCLZ */
+//   filter::ExternalFilter blosc_filter(FILTER_BLOSC, cd_values);
 
-  filter::ExternalFilter blosc_filter(FILTER_BLOSC, cd_values);
-
-  dataspace::Simple space{{0, xdim, ydim},{dataspace::Simple::unlimited, xdim, ydim}};
-  property::LinkCreationList lcpl;
-  property::DatasetAccessList dapl;
-  property::DatasetTransferList dtpl;
-  property::DatasetCreationList dcpl;
-  dcpl.layout(property::DatasetLayout::Chunked);
-  dcpl.chunk({1, xdim, ydim});
-  blosc_filter(dcpl);
+//   dataspace::Simple space{{0, xdim, ydim},{dataspace::Simple::unlimited, xdim, ydim}};
+//   property::LinkCreationList lcpl;
+//   property::DatasetAccessList dapl;
+//   property::DatasetTransferList dtpl;
+//   property::DatasetCreationList dcpl;
+//   dcpl.layout(property::DatasetLayout::Chunked);
+//   dcpl.chunk({1, xdim, ydim});
+//   blosc_filter(dcpl);
 
 
-  std::vector<int> frame(xdim * ydim, 0);
-  std::generate(frame.begin(), frame.end(), [](){return static_cast<int>(std::rand() % 100);});
+//   std::vector<int> frame(xdim * ydim, 0);
+//   std::generate(frame.begin(), frame.end(), [](){return static_cast<int>(std::rand() % 100);});
 
-  dataspace::Hyperslab framespace{{0, 0, 0}, {1, xdim, ydim}};
+//   dataspace::Hyperslab framespace{{0, 0, 0}, {1, xdim, ydim}};
 
-  GIVEN("an integer dataset") {
-    node::Dataset dataset(root, "data1", datatype::create<int>(),
-                          space, lcpl, dcpl, dapl);
-    WHEN("writing the data with external filter") {
-      for (long long unsigned int i = 0; i != nframe; i++) {
-        dataset.extent(0, 1);
-        framespace.offset(0,i);
-        dataset.write(frame, framespace);
-      }
-      THEN("read the data back") {
-        std::vector<int> read_value(xdim * ydim);
-        for (long long unsigned int i = 0; i != nframe; i++) {
-          framespace.offset({i, 0, 0});
-          dataset.read(read_value, framespace);
-          REQUIRE(frame == read_value);
-          ;
-        }
-      }
-    }
-  }
-}
+//   GIVEN("an integer dataset") {
+//     node::Dataset dataset(root, "data1", datatype::create<int>(),
+//                           space, lcpl, dcpl, dapl);
+//     WHEN("writing the data with external filter") {
+//       for (long long unsigned int i = 0; i != nframe; i++) {
+//         dataset.extent(0, 1);
+//         framespace.offset(0,i);
+//         dataset.write(frame, framespace);
+//       }
+//       THEN("read the data back") {
+//         std::vector<int> read_value(xdim * ydim);
+//         for (long long unsigned int i = 0; i != nframe; i++) {
+//           framespace.offset({i, 0, 0});
+//           dataset.read(read_value, framespace);
+//           REQUIRE(frame == read_value);
+//           ;
+//         }
+//       }
+//     }
+//   }
+// }
