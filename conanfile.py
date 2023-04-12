e90fcaed1e39228a9cffa0954f2d8d2c4a6ee5c7
Jan Kotanski <jankotan@gmail.com>
2023-04-12 17:16:19 +0200

    clean conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -24,21 +24,8 @@ class H5CppConan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("catch2/3.3.2")
-        # self.build_requires("catch2/2.13.10")
         self.build_requires("ninja/1.10.2")
         self.build_requires("zlib/1.2.13")
-        # self.build_requires("cmake/3.25.3")
-        if self.settings.os == "Windows":
-            self.build_requires("hdf5/1.14.0")
-            # self.build_requires("hdf5/1.12.2")
-            self.build_requires("libiconv/1.17")
-            self.build_requires("szip/2.1.1")
-            self.build_requires("bzip2/1.0.8")
-            if self.options.get_safe("with_boost", False):
-                self.build_requires("boost/1.81.0")
-            if self.options.get_safe("with_mpi", False):
-                self.build_requires("openmpi/4.1.0")
-
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -49,10 +36,8 @@ class H5CppConan(ConanFile):
             self.options["hdf5"].parallel = True
 
     def requirements(self):
-        # self.requires("hdf5/1.12.2")
         self.requires("hdf5/1.14.0")
         self.requires("catch2/3.3.2")
-        # self.requires("catch2/2.13.10")
         self.requires("libiconv/1.17")
         self.requires("zlib/1.2.13")
         self.requires("szip/2.1.1")
@@ -76,6 +61,8 @@ class H5CppConan(ConanFile):
             with run_env.apply():
                 cmake.configure(variables={
                     "H5CPP_CONAN": "MANUAL",
-                    "H5CPP_WITH_MPI": self.options.get_safe("with_mpi", False),
-                    "H5CPP_WITH_BOOST": self.options.get_safe("with_boost", False)})
+                    "H5CPP_WITH_MPI":
+                    self.options.get_safe("with_mpi", False),
+                    "H5CPP_WITH_BOOST":
+                    self.options.get_safe("with_boost", False)})
                 cmake.build()
